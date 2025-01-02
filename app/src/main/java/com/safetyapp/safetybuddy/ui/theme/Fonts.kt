b007dd283eb8c0a26b1595c4391b2eefcecf5006
uduak <uduakumanah10@gmail.com>
2025-01-02 14:51:21 +0000

    setting up design system

---
+++ app/src/main/java/com/safetyapp/safetybuddy/ui/theme/Fonts.kt
@@ -0,0 +1,13 @@
+package com.safetyapp.safetybuddy.ui.theme
+
+import androidx.compose.ui.text.font.Font
+import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.text.font.FontWeight
+import com.safetyapp.safetybuddy.R
+
+val OpenSans = FontFamily(
+    Font(R.font.roboto_bold,FontWeight.Normal),
+    Font(R.font.opensans_bold, FontWeight.Normal),
+    Font(R.font.opensans_regular,FontWeight.SemiBold),
+    Font(R.font.opensans_semibold,FontWeight.Bold),
+)
